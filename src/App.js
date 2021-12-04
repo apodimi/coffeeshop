842a9485cc8816a9074c89e34d31f68460798c76
Apos_Dimit <aposdimit@gmail.com>
2021-12-05 01:42:14 +0200

    New

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
         {shops.map(item => (
           <div class="max-w-sm rounded overflow-hidden shadow-lg">
           <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">{error.message}</div>
+            <div class="font-bold text-xl mb-2">{item.results}</div>
             <p class="text-gray-700 text-base">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
             </p>
