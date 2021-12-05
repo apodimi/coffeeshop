97e3c0d3286dd8c2aefb55f9a3aa10362845ef18
Apos_Dimit <aposdimit@gmail.com>
2021-12-05 17:46:09 +0200

    API Settings

--- src/App.js
+++ src/App.js
@@ -5,23 +5,13 @@ import HomePageHeader from './components/HomePageHeader';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [shops, setShops] = useState([]);
+
 
   useEffect(() => {
-    fetch("https://maps.googleapis.com/maps/api/place/textsearch/json?query=cafe%in%Athens&key=AIzaSyDZOB6FoiTv_3JOQvtLhc2L-QW7j4wy96g", {mode: "no-cors"})
-      .then(res =>  {return res.json()})
-      .then((result) => {
-        setIsLoaded(true);
-        setShops(result);
-        console.log(result);
-      },
-      (error) => {
-        setIsLoaded(true);
-        setError(error);
-      })
-  })
+    fetch("https://maps.googleapis.com/maps/api/place/textsearch/json?query=cafe%in%Athens&key=AIzaSyDZOB6FoiTv_3JOQvtLhc2L-QW7j4wy96g")
+      .then(res =>   console.log(res));
+      
+  }, [])
 
   return (
     <div className="App">
@@ -29,10 +19,10 @@ function App() {
       <HomePageHeader />
       <div className="mt-12 rounded-lg grid grid-cols-2 gap-4 max-w-7xl mx-auto pl-8  sm:px-16">
         <h1 className="text-black text-left text-extrabold text-4xl font-bold my-12">Athens</h1>
-        {shops.map(item => (
+        
           <div class="max-w-sm rounded overflow-hidden shadow-lg">
           <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">{item.results}</div>
+            <div class="font-bold text-xl mb-2">{}</div>
             <p class="text-gray-700 text-base">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
             </p>
@@ -43,7 +33,7 @@ function App() {
             <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
           </div>
         </div>
-        ))}
+        
       </div>
     </div>
   );
